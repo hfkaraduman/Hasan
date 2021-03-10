62fe20a69cfce4dad42a63ec00c59ffc498f38c5
Hasan <hfkaraduman@gmail.com>
2021-03-10 20:34:19 +0300

    10.03.2021
    Applicant Assesment

---
+++ Assesmenthasankaraduman/Assesmenthasankaraduman/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Assesmenthasankaraduman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.regular = new System.Windows.Forms.Button();
+            this.ara = new System.Windows.Forms.Button();
+            this.degistir = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dosya_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.esc = new System.Windows.Forms.Button();
+            this.sil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(475, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(475, 374);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(313, 35);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Visible = false;
+            // 
+            // regular
+            // 
+            this.regular.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.regular.Location = new System.Drawing.Point(475, 229);
+            this.regular.Name = "regular";
+            this.regular.Size = new System.Drawing.Size(313, 46);
+            this.regular.TabIndex = 3;
+            this.regular.Text = "Regular Tanımla";
+            this.regular.UseVisualStyleBackColor = true;
+            this.regular.Visible = false;
+            this.regular.Click += new System.EventHandler(this.regular_Click);
+            // 
+            // ara
+            // 
+            this.ara.Location = new System.Drawing.Point(475, 302);
+            this.ara.Name = "ara";
+            this.ara.Size = new System.Drawing.Size(313, 46);
+            this.ara.TabIndex = 7;
+            this.ara.Text = "Ara";
+            this.ara.UseVisualStyleBackColor = true;
+            this.ara.Visible = false;
+            this.ara.Click += new System.EventHandler(this.ara_Click);
+            // 
+            // degistir
+            // 
+            this.degistir.Location = new System.Drawing.Point(475, 440);
+            this.degistir.Name = "degistir";
+            this.degistir.Size = new System.Drawing.Size(313, 46);
+            this.degistir.TabIndex = 9;
+            this.degistir.Text = "Değiştir";
+            this.degistir.UseVisualStyleBackColor = true;
+            this.degistir.Visible = false;
+            this.degistir.Click += new System.EventHandler(this.degistir_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(475, 508);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(313, 35);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(457, 719);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8127, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(5697, 12299);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Arama Yapmak İçin =>F\r\nDeğiştirme Yapmak İçin =>R\r\nSilme Yapmak İçin=>D\r\n\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dosya_text
+            // 
+            this.dosya_text.Location = new System.Drawing.Point(475, 25);
+            this.dosya_text.Name = "dosya_text";
+            this.dosya_text.Size = new System.Drawing.Size(313, 26);
+            this.dosya_text.TabIndex = 15;
+            this.dosya_text.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dosya_text_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(484, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 40);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Dosya ismini girip Entera basınız\r\nÖrneğin  =>radar";
+            // 
+            // esc
+            // 
+            this.esc.Location = new System.Drawing.Point(595, 652);
+            this.esc.Name = "esc";
+            this.esc.Size = new System.Drawing.Size(75, 63);
+            this.esc.TabIndex = 18;
+            this.esc.Text = "Çıkış";
+            this.esc.UseVisualStyleBackColor = true;
+            this.esc.Visible = false;
+            this.esc.Click += new System.EventHandler(this.esc_Click);
+            // 
+            // sil
+            // 
+            this.sil.Location = new System.Drawing.Point(475, 571);
+            this.sil.Name = "sil";
+            this.sil.Size = new System.Drawing.Size(313, 38);
+            this.sil.TabIndex = 19;
+            this.sil.Text = "Sil";
+            this.sil.UseVisualStyleBackColor = true;
+            this.sil.Visible = false;
+            this.sil.Click += new System.EventHandler(this.sil_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 786);
+            this.Controls.Add(this.sil);
+            this.Controls.Add(this.esc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dosya_text);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.degistir);
+            this.Controls.Add(this.ara);
+            this.Controls.Add(this.regular);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button regular;
+        private System.Windows.Forms.Button ara;
+        private System.Windows.Forms.Button degistir;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dosya_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button esc;
+        private System.Windows.Forms.Button sil;
+    }
+}
+
---
+++ Assesmenthasankaraduman/Assesmenthasankaraduman/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Assesmenthasankaraduman
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public string dosya_yeri=Application.StartupPath;
+        public string dosya_ismi;
+        
+        
+        public string metin;
+        public string degistirilecek_metin;
+        public string Re;
+
+        public string sorgulanacak_kelime;
+        int sayici=0;
+        
+
+        public String  Regular_tanimla(string deger)
+        {
+            string re = "";
+            if (deger[0] != '-' && deger[0] != '*')
+            {
+                re += '^';
+            }
+            else if (deger[0] == '-')
+            {
+                re += '^';
+            }
+            else if (deger[0] == '*')
+            {
+                re += '^';
+            }
+
+            for (int i = 0; i < deger.Length; i++)
+            {
+
+                if (deger[i] == '-')
+                {
+                    re += "[a-zA-Z]";
+                }
+                //çğıöşüÇĞİÖŞÜ
+                else if (deger[i] == '*')
+                {
+                    re += "[a-zA-Z]*";
+                }
+
+                else
+                {
+                    re += deger[i];
+                }
+
+
+
+            }
+
+            if (deger[deger.Length - 1] != '-' && deger[deger.Length - 1] != '*')
+            {
+                re += "$";
+            }
+            else if (deger[deger.Length - 1] == '-')
+            {
+                re += "$";
+            }
+            else if (deger[deger.Length - 1] == '*')
+            {
+                re += "$";
+            }
+
+            return re;
+        }
+
+
+        public void regular_Click(object sender, EventArgs e)
+        {
+            Re = Regular_tanimla(textBox1.Text);
+
+            regular.Text = Re;
+        }
+
+         public void Metni_getir(string dosya)
+        {
+            dosya_ismi = dosya;
+            StreamReader streamReader = File.OpenText(Application.StartupPath + "\\\\"+dosya+".txt");
+            string okunan;
+                        
+            while ((okunan = streamReader.ReadLine()) != null)
+            {
+                metin +=okunan;
+            }
+            richTextBox1.Text = metin;
+            streamReader.Close();
+        }
+
+        public void ara_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+            Re = Regular_tanimla(textBox1.Text);
+            sorgulanacak_kelime = "";
+            regular.Text = Re;
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+            sayici = 0;
+            ArrayList kelimeler = new ArrayList();
+            int metin_uzunluk = metin.Length;
+            for(int i=0;i<metin_uzunluk;i++)
+            {
+
+                //if(metin[i]!=' ' && metin[i]!=',' && metin[i]!='.' && metin[i] != ';' && metin[i] != ':' && metin[i] != '/')
+                if (char.IsLetter(metin[i]) == true)
+                {
+                    sorgulanacak_kelime += metin[i];
+                }
+                else
+                    
+                {
+                    kelimeler.Add(sorgulanacak_kelime);
+                    Ara(sorgulanacak_kelime);
+                    sorgulanacak_kelime = "";
+                }
+            }
+            
+        }
+
+        public void Ara(string sorgulanacak_kelime)
+        {
+
+            Regex reg = new Regex(Re);
+
+            bool sonuc = reg.IsMatch(sorgulanacak_kelime);
+
+            
+            if (sonuc == true)
+            {
+                sayici += 1;
+
+                int start = 0;
+                int end = richTextBox1.Text.LastIndexOf(sorgulanacak_kelime);//sorgulanacak_kelimenin en son bulundugu yerin başlangıç indexi
+
+
+                while (start <= end)
+                {
+
+                    richTextBox1.Find(sorgulanacak_kelime, start, richTextBox1.TextLength, RichTextBoxFinds.WholeWord);
+
+
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+
+                    start = richTextBox1.Text.IndexOf(sorgulanacak_kelime, start) + 1;
+
+                }
+                textBox2.Text = sayici.ToString();
+            }
+
+        }
+
+        public void degistir_Click(object sender, EventArgs e)
+        {
+            Re = Regular_tanimla(textBox1.Text);
+
+            regular.Text = Re;
+            string degistirilecek_kelime = textBox3.Text;
+            degistirilecek_metin = metin;
+            sorgulanacak_kelime = "";
+
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+            
+            
+            int metin_uzunluk = metin.Length;
+            for (int i = 0; i < metin_uzunluk; i++)
+            {
+                if (metin[i] != ' ' && metin[i] != ',' && metin[i] != '.' && metin[i] != ';' && metin[i] != ':')
+                {
+                    sorgulanacak_kelime += metin[i];
+                }
+                else
+
+                {
+                    Degistir(sorgulanacak_kelime,degistirilecek_kelime);
+                    sorgulanacak_kelime = "";
+                }
+                
+                
+            }
+            richTextBox1.Clear();
+            richTextBox1.SelectAll();
+            metin = degistirilecek_metin;
+            richTextBox1.Text = metin;
+            
+
+        }
+
+        public void Degistir(string sorgulanacak_kelime,string degistirilecek_kelime)
+        {
+            Regex reg = new Regex(Re);
+            
+
+            bool sonuc = reg.IsMatch(sorgulanacak_kelime);
+
+            //label1.Text = sonuc.ToString();
+            if (sonuc == true)
+            {
+                degistirilecek_metin=degistirilecek_metin.Replace(sorgulanacak_kelime, degistirilecek_kelime);
+            }
+            
+        }
+
+        public void ara_Click_1(object sender, EventArgs e)
+        {
+            Re = Regular_tanimla(textBox1.Text);
+
+            regular.Text = Re;
+
+            degistirilecek_metin = metin;
+            sorgulanacak_kelime = "";
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+
+
+            int metin_uzunluk = metin.Length;
+            for (int i = 0; i < metin_uzunluk; i++)
+            {
+                if (metin[i] != ' ' && metin[i] != ',' && metin[i] != '.' && metin[i] != ';' && metin[i] != ':')
+                {
+                    sorgulanacak_kelime += metin[i];
+                }
+                else
+
+                {
+                    Degistir(sorgulanacak_kelime, "");
+                    sorgulanacak_kelime = "";
+                }
+
+
+            }
+            richTextBox1.Clear();
+            richTextBox1.SelectAll();
+            metin = degistirilecek_metin;
+            richTextBox1.Text = metin;
+
+
+        }
+
+
+
+        private void dosya_text_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                string dosya = dosya_text.Text;
+                Metni_getir(dosya);
+                dosya_text.Visible = false;
+                label1.Visible = false;
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                regular.Visible = true;
+                ara.Visible = true;
+                degistir.Visible = true;
+                sil.Visible = true;
+                esc.Visible = true;
+            }
+            
+        }
+
+        private void esc_Click(object sender, EventArgs e)
+        {
+            StreamWriter streamwriter = new StreamWriter(Application.StartupPath + "\\\\" + dosya_ismi + ".txt");
+            streamwriter.Write(metin);
+            streamwriter.Close();
+            richTextBox1.Clear();
+            Application.Exit();
+        }
+
+        public void sil_Click_1(object sender, EventArgs e)
+        {
+            Re = Regular_tanimla(textBox1.Text);
+
+            regular.Text = Re;
+
+            degistirilecek_metin = metin;
+            sorgulanacak_kelime = "";
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+
+
+            int metin_uzunluk = metin.Length;
+            for (int i = 0; i < metin_uzunluk; i++)
+            {
+                if (metin[i] != ' ' && metin[i] != ',' && metin[i] != '.' && metin[i] != ';' && metin[i] != ':')
+                {
+                    sorgulanacak_kelime += metin[i];
+                }
+                else
+
+                {
+                    Degistir(sorgulanacak_kelime, "");
+                    sorgulanacak_kelime = "";
+                }
+
+
+            }
+            richTextBox1.Clear();
+            richTextBox1.SelectAll();
+            metin = degistirilecek_metin;
+            richTextBox1.Text = metin;
+        }
+    }
+}
